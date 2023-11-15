af6b0ee956d0d9200ada1357c83a72c349a27289
Magomed Aliev <magomed157893@gmail.com>
2023-11-15 09:40:51 +0300

    Modified views

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,51 @@
 <template>
   <main>
     <section>
-      Home
+      <SidebarWidget title="Жанры">
+        <SidebarWidgetGenreBtn v-for="(item, key) in Object.keys(genres)" :key="key">
+          {{ genres[item] }}
+        </SidebarWidgetGenreBtn>
+      </SidebarWidget>
+      <ContentWrapper>
+      </ContentWrapper>
     </section>
   </main>
 </template>
+
+<script>
+import SidebarWidget from "@/components/SidebarWidget.vue";
+import SidebarWidgetGenreBtn from "@/components/SidebarWidgetGenreBtn.vue";
+import ContentWrapper from "@/components/ContentWrapper.vue";
+
+export default {
+  components: {
+    SidebarWidget,
+    SidebarWidgetGenreBtn,
+    ContentWrapper
+  },
+  data() {
+    return {
+      genres: {
+        business: "Бизнес",
+        detective: "Детектив",
+        health: "Здоровье",
+        history: "История",
+        classic: "Классика",
+        science: "Наука",
+        non_fiction: "Нон-фикшн",
+        education: "Образование",
+        adventure: "Приключение",
+        psychology: "Психология",
+        self_development: "Саморазвитие",
+        technic: "Техника",
+        thriller: "Триллер",
+        fantastic: "Фантастика",
+        fantasy: "Фэнтези",
+        philosophy: "Философия",
+        hobby: "Хобби",
+        humor: "Юмор"
+      }
+    }
+  }
+}
+</script>
--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,7 +1,22 @@
 <template>
   <main>
     <section>
-      Profile
+      <SidebarWidget title="Профиль">
+      </SidebarWidget>
+      <ContentWrapper>
+      </ContentWrapper>
     </section>
   </main>
 </template>
+
+<script>
+import SidebarWidget from "@/components/SidebarWidget.vue";
+import ContentWrapper from "@/components/ContentWrapper.vue";
+
+export default {
+  components: {
+    SidebarWidget,
+    ContentWrapper
+  }
+}
+</script>
