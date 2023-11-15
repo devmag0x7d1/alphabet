0543da851651a0029a4f7e48c427c8ec05e671dc
Magomed Aliev <magomed157893@gmail.com>
2023-11-15 09:09:39 +0300

    Created TheHeader, The Footer and added more general styles

---
+++ client/src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <section>
+      <div>
+        <p class="text-14">
+          @author: Magomed Aliev
+        </p>
+      </div>
+      <div>
+        <a class="text-14 text-btn" href="https://github.com/devmag0x7d1" target="_blank">
+          github.com/devmag0x7d1
+        </a>
+      </div>
+    </section>
+  </footer>
+</template>
---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <section>
+      <div>
+        <h1 class="text-32">
+          <a href="/">
+            Alphabet
+          </a>
+        </h1>
+      </div>
+      <div>
+        <button class="text-22 text-btn">
+          Login
+        </button>
+      </div>
+    </section>
+  </header>
+</template>
