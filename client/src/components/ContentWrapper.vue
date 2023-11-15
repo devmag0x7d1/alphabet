77968b7ac23f38d6dd478aea7fbcb9ae5b9a4e11
Magomed Aliev <magomed157893@gmail.com>
2023-11-15 09:39:36 +0300

    Created Wrapper for main

---
+++ client/src/components/ContentWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="content_wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.content_wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+}
+</style>
