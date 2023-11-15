fbe2735ebf5cb57a8f465437d61af5d5a2e69959
Magomed Aliev <magomed157893@gmail.com>
2023-11-15 09:37:52 +0300

    Created a SidebarWidget and in addition to it created buttons for HomeView

---
+++ client/src/components/SidebarWidget.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="sidebar_widget">
+    <div class="sidebar_widget-head">
+      <div>
+        <h2>
+          {{ this.title }}
+        </h2>
+      </div>
+      <div class="sidebar_widget-list">
+        <RouterLink :to="{ name: 'Home' }">
+          <BookIcon />
+        </RouterLink>
+        <RouterLink :to="{ name: 'Search' }">
+          <SearchIcon />
+        </RouterLink>
+        <RouterLink :to="{ name: 'Profile' }">
+          <ProfileIcon />
+        </RouterLink>
+      </div>
+    </div>
+    <div class="sidebar_widget-view">
+      <div class="sidebar_widget-wrapp">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BookIcon from "@/components/icons/BookIcon.vue"
+import SearchIcon from "@/components/icons/SearchIcon.vue"
+import ProfileIcon from "@/components/icons/ProfileIcon.vue"
+
+export default {
+  components: {
+    BookIcon,
+    SearchIcon,
+    ProfileIcon
+  },
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style>
+.sidebar_widget {
+  min-width: 270px;
+  max-width: 270px;
+  align-self: flex-start;
+}
+
+.sidebar_widget-head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: #f0e5da;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.15);
+}
+
+.sidebar_widget-list {
+  display: flex;
+  gap: 5px;
+  height: 30px;
+}
+
+.sidebar_widget-list>a {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 5px;
+  background-color: #ffffff;
+  box-shadow: inset 0 0 4px 0 rgba(0, 0, 0, 0.25);
+}
+
+.sidebar_widget-list>a:hover {
+  box-shadow: inset 0 0 8px 0 rgba(0, 0, 0, 0.25);
+}
+
+.sidebar_widget-list>a:active {
+  box-shadow: inset 0 0 4px 0 rgba(0, 0, 0, 0.25);
+}
+
+.sidebar_widget-list>a.router-link-active {
+  position: relative;
+}
+
+.sidebar_widget-list>a.router-link-active::before {
+  content: "";
+  position: absolute;
+  top: 33px;
+  left: 50%;
+  transform: translateX(-50%) rotate(45deg);
+  display: block;
+  width: 10px;
+  height: 10px;
+  background-color: #fffaf5;
+}
+
+.sidebar_widget-view {
+  position: relative;
+}
+
+.sidebar_widget-view::after {
+  content: "";
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 10px;
+  background-color: #f0e5da;
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.15);
+}
+
+.sidebar_widget-wrapp {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
---
+++ client/src/components/SidebarWidgetGenreBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="sidebar_widget-genre-btn text-14">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.sidebar_widget-genre-btn {
+  display: flex;
+  align-items: center;
+  width: calc(50% - 5px);
+  height: 30px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0 0 2px 0 rgba(0,0,0,0.15);
+  user-select: none;
+  cursor: pointer;
+}
+
+.sidebar_widget-genre-btn:hover {
+  box-shadow: 0 0 8px 0 rgba(0,0,0,0.15);
+}
+
+.sidebar_widget-genre-btn:active {
+  box-shadow: 0 0 2px 0 rgba(0,0,0,0.15);
+}
+</style>
